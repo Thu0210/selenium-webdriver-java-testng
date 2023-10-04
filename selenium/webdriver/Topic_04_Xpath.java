0c4abd2099879ef44eb6f2c9a34b8fc8fa7011f2
Thu Le <lethu02101996@gmail.com>
2023-10-04 21:40:51 +0700

    topic_06/07

--- selenium/webdriver/Topic_04_Xpath.java
+++ selenium/webdriver/Topic_04_Xpath.java
@@ -13,17 +13,9 @@ import java.util.concurrent.TimeUnit;
 
 public class Topic_04_Xpath {
     WebDriver driver;
-    String projectPath = System.getProperty("user.dir");
-    String osName = System.getProperty("os.name");
 
     @BeforeClass
     public void beforeClass() {
-        if (osName.contains("Windows")) {
-            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-        } else {
-            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
-        }
-
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
