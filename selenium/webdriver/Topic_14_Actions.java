9d22fe48d2834df88f0d06a48997a6336f4aec31
Thu Le <lethu02101996@gmail.com>
2023-11-18 17:18:30 +0700

    Topic_14_Part_III

--- selenium/webdriver/Topic_14_Actions.java
+++ selenium/webdriver/Topic_14_Actions.java
@@ -1,27 +1,37 @@
 package webdriver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_14_Actions {
     WebDriver driver;
     Actions actions;
+    JavascriptExecutor javascriptExecutor;
 
     @BeforeClass
     public void beforeClass() {
         driver = new FirefoxDriver();
+        //driver = new ChromeDriver();
         actions = new Actions(driver);
+        javascriptExecutor = (JavascriptExecutor) driver;
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
@@ -91,6 +101,88 @@ public class Topic_14_Actions {
         }
         Assert.assertEquals(allNumberTextExpected, allNumberTextActual);
 
+    }
+    @Test
+    public void TC_05_Double_Click(){
+        driver.get("https://automationfc.github.io/basic-form/index.html");
+        WebElement doubleClickElement = driver.findElement(By.xpath("//button[text()='Double click me']"));
+        if(driver.toString().contains("firefox")){
+            //scrollIntoView = true: keo mep tren cua element tren phia tren cung cua viewport
+            // scrollIntoView = false: keo mep duoi cua element phia duoi cung cua viewport
+            javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", doubleClickElement);
+            sleepInSecond(3);
+        }
+            actions.doubleClick(doubleClickElement).perform();
+            sleepInSecond(2);
+
+        Assert.assertEquals(driver.findElement(By.xpath("//p[@id='demo']")).getText(), "Hello Automation Guys!");
+
+    }
+    @Test
+    public void TC_06_Right_Click() {
+        driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.context-menu-icon-paste")).isDisplayed());
+        // contextClick = right click
+        actions.contextClick(driver.findElement(By.cssSelector("span.context-menu-one"))).perform();
+        sleepInSecond(3);
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.context-menu-icon-paste")).isDisplayed());
+        actions.moveToElement(driver.findElement(By.cssSelector("li.context-menu-icon-paste"))).perform();
+        sleepInSecond(3);
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.context-menu-icon-paste.context-menu-hover.context-menu-visible")).isDisplayed());
+        actions.click(driver.findElement(By.cssSelector("li.context-menu-icon-paste"))).perform();
+        sleepInSecond(3);
+        driver.switchTo().alert().accept();
+        sleepInSecond(3);
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.context-menu-icon-paste")).isDisplayed());
+
+
+    }
+    @Test
+    public void TC_07_Drag_And_Drop_HTML4() {
+        driver.get("https://automationfc.github.io/kendo-drag-drop/");
+        WebElement sourceCircle = driver.findElement(By.cssSelector("div#draggable"));
+        WebElement targetCircle = driver.findElement(By.cssSelector("div#droptarget"));
+        actions.dragAndDrop(sourceCircle,targetCircle).pause(3000).perform();
+        sleepInSecond(3);
+        Assert.assertEquals(targetCircle.getText(), "You did great!");
+        Assert.assertEquals(Color.fromString(targetCircle.getCssValue("background-color")).asHex().toLowerCase(), "#03a9f4");
+
+
+    }
+    @Test
+    public void TC_08_Drag_And_Drop_HTML5_CSS() throws IOException {
+        driver.get("https://automationfc.github.io/drag-drop-html5/");
+        WebElement columnA = driver.findElement(By.cssSelector("div#column-a"));
+        WebElement columnB = driver.findElement(By.cssSelector("div#column-b"));
+        String projectPath = System.getProperty("user.dir");
+        String dragAndDropFilePath = projectPath + "/DragAndDrop/Helper.js";
+        String jsContentFile = getContentFile(dragAndDropFilePath);
+        //Thuc thi doan lenh JS
+        javascriptExecutor.executeScript(jsContentFile);
+        sleepInSecond(5);
+
+
+    }
+    @Test
+    public void TC_09_Drag_And_Drop_HTML5_Xpath() {
+        driver.get("https://automationfc.github.io/drag-drop-html5/");
+
+    }
+    public String getContentFile(String filePath) throws IOException {
+        Charset cs = Charset.forName("UTF-8");
+        FileInputStream stream = new FileInputStream(filePath);
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+            StringBuilder builder = new StringBuilder();
+            char[] buffer = new char[8192];
+            int read;
+            while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+                builder.append(buffer, 0, read);
+            }
+            return builder.toString();
+        } finally {
+            stream.close();
+        }
     }
 
     @AfterClass
